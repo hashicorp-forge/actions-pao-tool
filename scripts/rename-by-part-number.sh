419f6ea7a71cfa00def63657f467998a5fbef1c3
ethel-hashicorp <ethel.evans@hashicorp.com>
2025-07-18 10:28:43 -0700

    SMRE 595: Rename-by-part-number script, action, and script tests (#5)
    * Adds script, action, and tests for renaming artifacts to their eBOM  part numbers

---
+++ scripts/rename-by-part-number.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+log() { echo "$*" 1>&2 ; }
+
+ERROR_COUNT=0
+ERROR_TOTAL=0
+err() { log "[error] $*" ; ERROR_COUNT=$((ERROR_COUNT + 1)) ; }
+
+rename_by_part_number() { local pao_dir="$1" input_dir="$2" output_dir="$3" 
+    
+    for ebom in "$pao_dir/v1/eboms/"*.csv; do
+        "${BASH_SOURCE%/*}/verify-required-files.sh" get-part-map "$ebom" > part-map.csv
+
+        # Link the files to their new filenames
+        while IFS=, read -r part_number filename; do
+            if [[ -z "$part_number" || -z "$filename" ]]; then
+                err "Part number or filename was empty - part number: '$part_number', filename: '$filename'"
+                continue
+            fi
+
+            extension="${filename##*.}"
+            if ! [ -f "$input_dir/$filename" ]; then
+                err "File '$filename' does not exist in input directory"
+                continue
+            fi
+
+            if ! ln -v "$input_dir/$filename" "$output_dir/$part_number.$extension"; then
+                err "Duplicate part number '$part_number' for file '$filename'."
+                continue
+            fi
+        done < "part-map.csv"
+
+        if [[ $ERROR_COUNT -gt 0 ]]; then
+            err "Encountered $ERROR_COUNT errors while processing part numbers for ebom '$ebom'."
+            ERROR_TOTAL=$((ERROR_TOTAL + ERROR_COUNT))
+            ERROR_COUNT=0
+        fi
+    done
+
+    if [[ $ERROR_TOTAL -gt 0 ]]; then
+        exit 1
+    fi
+}
+
+main() {
+    set -euo pipefail
+    rename_by_part_number "$@"
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    main "$@"
+fi
---
+++ tests/rename-by-part-number/rename-by-part-number.bats
@@ -0,0 +1,41 @@
+#!/usr/bin/env bats
+
+# Need 1.7.0 for run() to accept flags
+# Note: need 1.7.0 for bats_require_minimum_version definition.
+bats_require_minimum_version 1.7.0
+
+# Rename bats' `run` function as `bats_run`.
+# This must be in the current shell, so it cannot be within a bats setup* function.
+eval "$(echo -n 'bats_run()' ; declare -f run | tail -n +2)"
+
+setup() {
+    bats_load_library bats-support
+    bats_load_library bats-assert
+
+    source scripts/rename-by-part-number.sh
+    export GITHUB_OUTPUT=/dev/null
+}
+
+@test "happy-path" {
+    tempdir="$(mktemp -d)"
+    bats_run --separate-stderr -- main "$BATS_TEST_DIRNAME/testdata/pao-metadata-files/happy-path" \
+        "$BATS_TEST_DIRNAME/testdata/input-happy-path" \
+        "$tempdir" 
+    assert_equal "$stderr" ""
+    assert [ -f "$tempdir/MOOCOWA.zip" ]
+    assert [ -f "$tempdir/MOOCOWB.zip" ]
+    assert_success
+}
+
+@test "multiple eboms" {
+    tempdir="$(mktemp -d)"
+    bats_run --separate-stderr -- main "$BATS_TEST_DIRNAME/testdata/pao-metadata-files/multiple-eboms" \
+        "$BATS_TEST_DIRNAME/testdata/input-happy-path" \
+        "$tempdir" 
+    assert_equal "$stderr" ""
+    assert [ -f "$tempdir/MOOCOWA.zip" ]
+    assert [ -f "$tempdir/MOOCOWB.zip" ]
+    assert [ -f "$tempdir/MOOCOW1.zip" ]
+    assert [ -f "$tempdir/MOOCOW2.zip" ]
+    assert_success
+}
